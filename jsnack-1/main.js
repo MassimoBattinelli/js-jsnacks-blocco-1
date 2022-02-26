a3338f0bb4c574bdd164e213f9f2b25b535ed393
massi <massimo.battinelli@gmail.com>
2022-02-26 12:06:32 +0100

    fine snack-1

---
+++ jsnack-1/main.js
@@ -0,0 +1,10 @@
+const num1 = parseInt(prompt ('Numero 1'));
+const num2 = parseInt(prompt ('Numero 2'));
+
+if (num1>num2) {
+    alert(num1);
+} else if (num1<num2) {
+    alert (num2);
+} else {
+    alert ('sono uguali')
+}
