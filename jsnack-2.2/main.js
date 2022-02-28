0ec0fe5034a5f854fe931f63a65de9bd58067c1f
massi <massimo.battinelli@gmail.com>
2022-02-28 16:42:53 +0100

    jsnack-2.2

---
+++ jsnack-2.2/main.js
@@ -0,0 +1,9 @@
+let UserNumber = parseInt(prompt('Inserisci un numero:'));
+
+if (UserNumber % 2 == 0){
+    alert(UserNumber + ' è pari.');
+} else {
+    UserNumber = UserNumber + 1
+    alert(UserNumber);
+    console.log (UserNumber);
+}
